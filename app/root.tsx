24e3f9452670136986129c9184b063e56fd76f96
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-05-05 22:15:53 +0900

    検索

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
   Form,
@@ -37,8 +37,12 @@ export const links: LinksFunction = () => [
 ];
 
 // Note:data.tsからデータを取得するローダー
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return json({ contacts });
 };
 
